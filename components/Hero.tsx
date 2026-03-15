8ac5dbd7ed1e1974972c8190b902257d84ad3faa
David Howe <65414349+bagodrago@users.noreply.github.com>
2026-03-15 16:48:51 -0400

    Implemented basic hero with background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
 
-const hero = () => {
+
+const Hero = () => {
   return (
-    <section className="hero min-h-screen">
-      <div className="hero-content">
-        
+    <section className="hero min-h-screen pt-17 flex-1 relative overflow-hidden">
+      {/*Background Image*/}
+      <div
+        className="absolute inset-0 bg-cover bg-center blur-sm scale-110 saturate-90 brightness-80"
+        style={{backgroundImage: "url('/marble-bg.jpg')"}}
+      />
+
+      <div className='hero-overlay'/>
+
+      <div className="hero-content text-neutral-content text-center">
+        <div className='max-w-md'>
+          <h1>TEST</h1>
+        </div>
       </div>
+      
     </section>
   )
 }
 
-export default hero
+export default Hero
