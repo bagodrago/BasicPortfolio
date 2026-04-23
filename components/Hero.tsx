59a207dbf7cf9c2a4808d8d97644e903dd9df2e3
David Howe <65414349+bagodrago@users.noreply.github.com>
2026-04-23 13:21:35 -0400

    Implemented ScrollLink to logo for smoother scrolling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import ScrollLink from "./ScrollLink";
 
 const Hero = () => {
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,23 +3,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import useTheme from '@/contexts/useTheme';
+import ScrollLink from './ScrollLink';
 
 const Logo = () => {
   const { isDark } = useTheme();
 
   return (
     <div className='btn btn-ghost py-6'>
-      <Link href="#" onClick={() => {window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      })}} replace>
+      <ScrollLink href="/">
         <Image 
           src="/logotk.svg" 
           alt="Logo" 
           width={100} height={80}
           className={'transition duration-300 ' + (isDark ? 'invert' : '')}
         />
-      </Link>
+      </ScrollLink>
     </div>
   )
 }
